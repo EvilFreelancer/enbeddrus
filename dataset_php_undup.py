ca888256b660cf0b331ee8e3a84c3903fd1b6bd3
Paul Rock <paul@drteam.rocks>
2024-06-09 02:32:09 +0300

    evilfreelancer/golang-en-ru dataset published

--- dataset_php_undup.py
+++ dataset_php_undup.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
-input_csv = './dataset/docs_php.clean.csv'
-output_csv = './dataset/docs_php.undup.csv'
+input_csv = './datasets/docs_go.clean.csv'
+output_csv = './datasets/docs_go.undup.csv'
 
 df = pd.read_csv(input_csv)
 
 # Only English
-df = df[df['English'] != df['Russian']]
+df = df[df['en'] != df['ru']]
 
 # Remove rows less than 10
-df = df[df['English'].str.len() > 15]
+df = df[df['en'].str.len() > 15]
 
 # Order by column name
-df = df.sort_values('English')
+df = df.sort_values('en')
+df.drop(['ID'], axis=1, inplace=True)
 
 # Add index
 df.index.name = 'id'
